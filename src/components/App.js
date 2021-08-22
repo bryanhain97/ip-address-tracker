9573ab8a9a0766148a9442d5949e476002766351
Bryan Hain <bryanhain97@gmail.com>
2021-08-22 11:43:46 +0200

    create react dev-environment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import '../styles/index.css';
+// import Navbar from "./Navbar"
+
 
 function App() {
   return (
     <div className="App">
-      hey man
+
     </div>
   );
 }
 
+/*
+<div id="root">
+  <div class="App">
+    <component1 />
+    <componentN />
+  </div>
+</div>
+*/
 export default App;
