14f020e1819bd8f5c9f1007b349936b9077039f7
Bryan Hain <bryanhain97@gmail.com>
2021-08-23 17:12:03 +0200

    ip tracker header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Navbar from '../Navbar/navbar';
+
 
 function Header (){
     return (
@@ -7,9 +9,10 @@ function Header (){
                 <h1 className="header-headline">IP Address Tracker</h1>
                 <form action="post" className="header-form">
                     <input className="header-input" type="text" placeholder="Search for any IP address or domain"></input>
-                    <button className="header-button"></button>
+                    <button className="header-button">&rsaquo;</button>
                 </form>
             </div>
+            <Navbar />
         </div>
     )
 }
